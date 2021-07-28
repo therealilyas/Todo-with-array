fb4f50273f52bb428eec2a65722018bcf5da00b1
Ilyas <ilyas.sultanov2020@list.ru>
2021-07-28 19:11:54 +0500

    Todo with Array

---
+++ js/script.js
@@ -0,0 +1,119 @@
+let addInput = document.getElementById('todoInput');
+let addBtn = document.getElementById('addToDoBtn');
+let todoUL = document.getElementById('todoList');
+let countTodo = document.getElementById('countTodo');
+let clearAllBtn = document.getElementById('clearToDoBtn');
+
+let todoArray = [];
+// Start 
+init();
+
+function init() {
+    // alert('Wlecome to the ToDo ');
+}
+
+
+// -= Fucking steps =-
+// 1) add element to fucking array
+
+// * 1  Keys event
+addInput.addEventListener('keyup', () => {
+
+    addBtn.classList.add("active");
+
+});
+addBtn.addEventListener('click', () => {
+    addTodo();
+});;
+
+addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        addTodo();
+    }
+});
+
+// * 2 Add Function
+function addTodo() {
+    if (addInput.value == "") {
+        addBtn.classList.remove('active');
+        return;
+    }
+
+    todoArray.push(addInput.value);
+    showToDo();
+}
+
+
+
+// 2) showToDo in display
+
+function showToDo() {
+
+    if (addInput.value == '') {
+        addBtn.classList.remove("active");
+    }
+
+    let todoLI = '';
+
+    todoArray.forEach((element, i) => {
+
+        todoLI += `
+        <li>
+        <span>${element}</span><button onclick="removeTodo(${i})">X</button>
+        </li>
+        `
+
+    });
+
+    todoUL.innerHTML = todoLI;
+
+    addInput.value = '';
+
+    addBtn.classList.remove("active");
+
+    countTodo.innerText = todoArray.length;
+
+    clearAllTodo();
+}
+
+
+// 3) delete item in array
+
+function removeTodo(i) {
+
+    console.log('Fucking array before deletion', todoArray);
+
+    delete todoArray[i];
+
+    todoArray = todoArray.filter((todoArray) => {
+        if (todoArray !== undefined) {
+            return todoArray;
+        }
+    });
+
+    console.log('Fucking array after deletion ', todoArray);
+    showToDo();
+}
+
+
+// 4) clear all todo 
+
+function clearAllTodo() {
+
+    if (todoArray.length > 0) {
+        clearAllBtn.classList.add('active');
+    } else {
+        clearAllBtn.classList.remove('active');
+    }
+
+    clearAllBtn.addEventListener('click', () => {
+
+        todoArray = [];
+        todoUL.innerHTML = '';
+        countTodo.innerText = 0;
+
+    });
+
+}
+
+// Finish !!!
